c0f69e359a7b585e9702cc8de7e0eb4865101cf6
AleFunky <alefunkyextra@gmail.com>
2026-06-18 12:26:59 +0200

    Blending enjoyment increased by 100%

--- source/graphics.c
+++ source/graphics.c
@@ -985,7 +985,12 @@ void change_blending(bool blending) {
             GPU_SRC_ALPHA, GPU_ONE,
             GPU_ONE, GPU_ZERO
         );
-        C2D_Prepare();
+
+        C3D_TexEnv *env = C3D_GetTexEnv(2);
+        C3D_TexEnvInit(env);
+        C3D_TexEnvSrc(env, C3D_Alpha, GPU_PREVIOUS, GPU_TEXTURE0, 0);
+        C3D_TexEnvFunc(env, C3D_Alpha, GPU_MODULATE);
+        //C2D_Prepare();
     } else {
         C2D_Flush();
         C3D_AlphaBlend(
